162917b772b5459eec2093b1ff33c75db463f282
Fedor <fedor.kochkin48@gmail.com>
2023-01-30 04:36:37 +0300

    создание функции для поиска строк с длинной меньше 3 и запись их в массив

--- ikr/Program.cs
+++ ikr/Program.cs
@@ -3,7 +3,22 @@
 for (int i = 0; i < s.Length; i++)
 Console.Write(" " + s[i]);
 }
+string[] OutputString(string[] s)
+{
+string[] mas = new string[s.Length];
+int k = 0;
+for (int i = 0; i < s.Length; i++)
+{
+if (s[i].Length <= 3)
+{
+mas[k] = s[i];
+k = k + 1;
+}
+}
+return mas;
+}
 Console.Clear();
 Console.Write("Введите массив: ");
 string [] s = Console.ReadLine().Split(" ");
 InputString(s);
+string [] vivod = OutputString(s);
