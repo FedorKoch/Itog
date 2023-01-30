b3c563682fad6d747e2c2301e7303c40aec94421
Fedor <fedor.kochkin48@gmail.com>
2023-01-30 04:31:53 +0300

    вывод изначального массива на экран

--- ikr/Program.cs
+++ ikr/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputString(string[] s)
+{
+for (int i = 0; i < s.Length; i++)
+Console.Write(" " + s[i]);
+}
+Console.Clear();
+Console.Write("Введите массив: ");
+string [] s = Console.ReadLine().Split(" ");
+InputString(s);
